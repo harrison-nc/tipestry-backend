50fd6528a834ef0e3e68b151110a6e49d02aeb23
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-12 17:59:28 +0100

    Test /api/users name length

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -19,5 +19,13 @@ describe('/api/users', () => {
 
             expect(res.status).toBe(400);
         });
+
+        it('should return 400 is name length is less than 4', async () => {
+            const res = await request(server)
+                .post('/api/users')
+                .send({ name: '123' });
+
+            expect(res.status).toBe(400);
+        });
     });
 });
