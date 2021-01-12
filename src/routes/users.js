c84c2f6eaf839b699c5c9cc302623dcf2cfeeac7
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-12 18:35:43 +0100

    Bugfix: convert req.body.name.length to a number

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     if (!req.body.name) return res.status(400).send();
-    if (!req.body.name.length < 4) return res.status(400).send();
+
+    let length = +req.body.name.length;
+    if (length < 4) return res.status(400).send();
 
     res.send();
 });
