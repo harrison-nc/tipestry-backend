ef8c3afd513a094d10dc276a39346715bbfbf68d
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-12 19:27:24 +0100

    Add middlware to validate request parameters

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const Joi = require('joi');
+const validator = require('../middleware/validateReqParameters');
 
 const router = express.Router();
+const validateInput = validator(validateUser);
 
-router.post('/', (req, res) => {
-    const { error } = validateUser(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
+router.post('/', validateInput, (req, res) => {
     res.status(200).send();
 });
 
