aa528b729e1fa8f0cd04e85685aa5305bee91894
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-13 14:31:06 +0100

    Test /api/posts returns 200

--- test/integration/post.test.js
+++ test/integration/post.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const User = require('../../src/db/user');
 
 describe('It works!', () => {
     let server;
@@ -12,8 +13,22 @@ describe('It works!', () => {
     });
 
     describe('POST /api/posts', () => {
+        let post;
+        let token;
 
-        const createPost = (data, header = 'x-auth-token', value = '') => {
+        beforeEach(() => {
+            post = {
+                title: 'post1',
+                resourceUrl: 'resource.com/myresource',
+                description: 'a description',
+                upVotes: 0,
+                downVotes: 0,
+                comments: [],
+            }
+            token = new User().generateAuthToken();
+        });
+
+        const createPost = (data, header = 'x-auth-token', value = token) => {
             return request(server)
                 .post('/api/posts')
                 .set(header, value)
@@ -21,15 +36,21 @@ describe('It works!', () => {
         };
 
         it('should return 401 if token is not provided', async () => {
-            const res = await createPost({}, 'x-invalid-header');
+            const res = await createPost(post, 'x-invalid-header');
 
             expect(res.status).toBe(401);
         });
 
         it('should return 400 if token is invalid', async () => {
-            const res = await createPost({}, 'x-auth-token', '1234');
+            const res = await createPost(post, 'x-auth-token', '1234');
 
             expect(res.status).toBe(400);
+        });
+
+        it('should return 200 if token is valid', async () => {
+            const res = await createPost(post, 'x-auth-token', token);
+
+            expect(res.status).toBe(200);
         });
     });
 });
