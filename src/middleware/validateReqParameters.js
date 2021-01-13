6f080080a8ab9ff9f699b575f41eeee9bc5e5e31
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-13 21:19:21 +0100

    Add function to validate logged in user in validator middleware

--- src/middleware/validateReqParameters.js
+++ src/middleware/validateReqParameters.js
@@ -1,8 +1,23 @@
-module.exports = function (validator) {
+module.exports = exports = function (validator) {
     return (req, res, next) => {
         const { error } = validator(req.body);
+
         if (error) return res.status(400).send(error.details);
 
         next();
     };
+}
+
+exports.withUser = function (validator) {
+    return (req, res, next) => {
+        const { activeUser, body } = req;
+
+        if (!activeUser) return res.status(400).send('User not logged in');
+
+        const { error } = validator(activeUser, body);
+
+        if (error) return res.status(400).send(error.details);
+
+        next();
+    }
 }
