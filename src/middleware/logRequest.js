3fe7add76ed0a7324578e69a3352a4190042d39d
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-20 19:44:59 +0100

    Add configuration to control logging of request

--- src/middleware/logRequest.js
+++ src/middleware/logRequest.js
@@ -1,4 +1,10 @@
+const config = require('config');
+
 module.exports = function (req, res, next) {
+	if (!config.get('logRequest')) {
+		next();
+		return;
+	}
 	console.log("Request", req.method, req.url);
 	console.log(req.body);
 	next();
