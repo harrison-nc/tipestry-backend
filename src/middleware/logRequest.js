2361d3bb89fc85613d6afd38a5b46323e5c17019
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-22 20:41:19 +0100

    Log request token header

--- src/middleware/logRequest.js
+++ src/middleware/logRequest.js
@@ -6,6 +6,7 @@ module.exports = function (req, res, next) {
 		return;
 	}
 	console.log("Request", req.method, req.url);
+	console.log('Token', req.headers['x-auth-token']);
 	console.log(req.body);
 	next();
 };
